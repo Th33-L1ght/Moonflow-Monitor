6ee81c4e0e37fed3f6a436c2fcff6cadff3389ab
Noora Salie <noorasalie@gmail.com>
2025-06-30 21:39:00 +0000

    make the icon like this but a little more subtle

--- src/components/PadsButterflyIcon.tsx
+++ src/components/PadsButterflyIcon.tsx
@@ -11,11 +11,12 @@ export const PadsButterflyIcon = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <rect x={11} y={3} width={2} height={18} rx={1} />
-    <path d="M12 21v2" />
-    <path d="M11 5C4.5 6.5 2 9.5 2 12s2.5 5.5 9 7" />
-    <path d="M6.5 9.5c-1.5 0-1.5 5 0 5" />
-    <path d="M13 5c6.5 1.5 9 4.5 9 7s-2.5 5.5-9 7" />
-    <path d="M17.5 9.5c1.5 0 1.5 5 0 5" />
+    <path d="M12 2v20" />
+    <path d="M10 4c-2 0-4-1-4-2" />
+    <path d="M14 4c2 0 4-1 4-2" />
+    <path d="M12 7c-4 0-8 1-8 5s4 5 8 5" />
+    <path d="M12 7c4 0 8 1 8 5s-4 5-8 5" />
+    <path d="M12 12c-4 0-7 1-7 3s3 3 7 3" />
+    <path d="M12 12c4 0 7 1 7 3s-3 3-7 3" />
   </svg>
 )
