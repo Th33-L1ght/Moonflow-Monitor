8f60d5c8ffbd82e8a5f23abb1205961ac7f0f085
Noora Salie <noorasalie@gmail.com>
2025-06-30 21:35:03 +0000

    make my app icon a bit more prettier so it actually look like a butterfl

--- src/components/PadsButterflyIcon.tsx
+++ src/components/PadsButterflyIcon.tsx
@@ -11,16 +11,11 @@ export const PadsButterflyIcon = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    {/* Body (Tampon) */}
-    <rect x={11} y={2} width={2} height={18} rx={1} />
-    <path d="M12 20v2" />
-
-    {/* Left Wing (Pad) */}
-    <path d="M11 7c-4 1-6 3-6 5s2 4 6 5" />
-    <path d="M7 9.5c-2 0-2 5 0 5" />
-
-    {/* Right Wing (Pad) */}
-    <path d="M13 7c4 1 6 3 6 5s-2 4-6 5" />
-    <path d="M17 9.5c2 0 2 5 0 5" />
+    <rect x={11} y={3} width={2} height={18} rx={1} />
+    <path d="M12 21v2" />
+    <path d="M11 5C4.5 6.5 2 9.5 2 12s2.5 5.5 9 7" />
+    <path d="M6.5 9.5c-1.5 0-1.5 5 0 5" />
+    <path d="M13 5c6.5 1.5 9 4.5 9 7s-2.5 5.5-9 7" />
+    <path d="M17.5 9.5c1.5 0 1.5 5 0 5" />
   </svg>
 )
