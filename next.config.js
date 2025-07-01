9ae3c33f3dcbbcac97fca6aa361a645cdc68495d
Noora Salie <noorasalie@gmail.com>
2025-07-01 09:33:25 +0000

    can you check the whole code . check all the errors and bugs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 
-import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
 
 const withPWA = require('next-pwa')({
   dest: 'public',
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
 });
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
@@ -22,4 +22,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
