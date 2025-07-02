5377b4babec510402e173727041d4bcfbbf6ea8c
Noora Salie <noorasalie@gmail.com>
2025-07-02 23:12:40 +0000

    erros plus i still cant add kids

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googletagmanager.com *.google-analytics.com; object-src 'none'; base-uri 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
