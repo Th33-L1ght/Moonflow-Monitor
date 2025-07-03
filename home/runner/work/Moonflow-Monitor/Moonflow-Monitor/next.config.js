bcbe8e562a9bda70f82314cb6650877fe4a3f281
Noora Salie <noorasalie@gmail.com>
2025-07-03 13:42:01 +0000

    Content Security Policy of your site blocks the use of 'eval' in JavaScr

--- home/runner/work/Moonflow-Monitor/Moonflow-Monitor/next.config.js
+++ home/runner/work/Moonflow-Monitor/Moonflow-Monitor/next.config.js
@@ -1,7 +1,32 @@
-
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http:;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: https://placehold.co;
+  connect-src *;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+`.replace(/\s{2,}/g, ' ').trim()
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy,
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
